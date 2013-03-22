15274ab8275318f9b4e05f25cd85d768e89f8cff
mshopsin+awoo <student@appacademy.io>
2013-03-21 18:28:49 -0700

    everything done

--- twitter_client.rb
+++ twitter_client.rb
@@ -10,8 +10,6 @@ CONSUMER_SECRET = "zRieLDh6VBpiHf7gieJrlrHw67EE2QSZg9795RC50"
 CONSUMER = OAuth::Consumer.new(
   CONSUMER_KEY, CONSUMER_SECRET, :site => "https://twitter.com")
 
-
-
 class Status
   attr_reader :user, :tweet
 
@@ -21,11 +19,8 @@ class Status
   end
 
   def post_status
-    a = user.access_token.post("http://api.twitter.com/1.1/statuses/update.json", {"status" => @tweet}).body
-
-    puts "#{a}"
+    a = EndUser.access_token.post("http://api.twitter.com/1.1/statuses/update.json", {"status" => @tweet}).body
   end
-
 end
 
 class User
@@ -66,13 +61,12 @@ class EndUser < User
 
   def self.request_token
     request_token = CONSUMER.get_request_token
-    authorize_url = EndUser.request_token.authorize_url
+    authorize_url = request_token.authorize_url
     puts "Go to this URL: #{authorize_url}"
     Launchy.open(authorize_url)
 
     puts "Login, and type your verification code in"
     oauth_verifier = gets.chomp
-
     access_token = request_token.get_access_token(
         :oauth_verifier => oauth_verifier)
   end
@@ -81,13 +75,12 @@ class EndUser < User
     if File.exist?(token_file)
       File.open(token_file) { |f| YAML.load(f) }
     else
-      access_token = request_token
+      access_token = EndUser.request_token
       File.open(token_file, "w") { |f| YAML.dump(access_token, f) }
 
       access_token
     end
   end
-
 
   def timeline
     tweets = []
@@ -106,9 +99,6 @@ class EndUser < User
   def tweet(message)
     status = Status.new(self, message)
     status.post_status
-
-    p status ###
-
     @statuses << status
   end
 
@@ -117,16 +107,14 @@ class EndUser < User
   end
 
   def dm(target_user, message)
-    a = Addressable::URI.new(
+    uri = Addressable::URI.new(
        :scheme => "https",
-       :host => "maps.googleapis.com",
-       :path => "/maps/api/directions/json",
-       :query_values => { :origin => from,
-                          :destination => to,
-                          :sensor => "false",
-                          :mode => "walking" }
+       :host => "api.twitter.com",
+       :path => "/1.1/direct_messages/new.json",
+       :query_values => { :text => message,
+                          :screen_name => target_user }
     )
-    dm = @@access_token.post("http://api.twitter.com/1.1/statuses/update.json", {"text" => message, "screen_name"  => target_user}).body
+    dm = @@access_token.post(uri.to_s).body
   end
 
   def get_friends ##rename?
